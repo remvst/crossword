1cb17be2760eb1bf13a83f3eee988a770ff55c67
remvst <remvst@gmail.com>
2024-11-22 00:24:42 -0500

    Grid navigation

--- client/src/components/grid-component.tsx
+++ client/src/components/grid-component.tsx
@@ -1,12 +1,22 @@
-import { Grid } from "@remvst/crossword";
-import React from "react";
+import { Grid, WordBounds } from "@remvst/crossword";
+import React, { useMemo, useState } from "react";
 import './grid-component.css';
 
 function Cell(props: {
     character: string,
+    selected: boolean,
+    highlighted: boolean,
     onClick: () => void,
 }) {
-    return <td onClick={props.onClick}>{props.character}</td>;
+    return <td
+        onClick={props.onClick}
+        className={[
+            props.selected ? 'selected' : '',
+            props.highlighted ? 'highlighted' : '',
+        ].join(' ')}
+    >
+        {props.character}
+    </td>;
 }
 
 function BlackCell(props: {}) {
@@ -16,6 +26,53 @@ function BlackCell(props: {}) {
 export function GridComponent(props: {
     grid: Grid,
 }) {
+    const [selectedRow, setSelectedRow] = useState(0);
+    const [selectedCol, setSelectedCol] = useState(0);
+    const [vertical, setVertical] = useState(false);
+
+    const highlight = useMemo<WordBounds>(() => {
+        let fromRow = selectedRow;
+        let fromCol = selectedCol;
+
+        let toRow = selectedRow;
+        let toCol = selectedCol;
+
+        if (vertical) {
+            while (fromRow > 0 && props.grid.cells[fromRow - 1][fromCol].dictionaryItems.vertical) {
+                fromRow--;
+            }
+            while (toRow < props.grid.rows - 1 && props.grid.cells[toRow + 1][fromCol].dictionaryItems.vertical) {
+                toRow++;
+            }
+        } else {
+            while (fromCol > 0 && props.grid.cells[fromRow][fromCol - 1].dictionaryItems.horizontal) {
+                fromCol--;
+            }
+            while (toCol < props.grid.cols - 1 && props.grid.cells[fromRow][toCol + 1].dictionaryItems.horizontal) {
+                toCol++;
+            }
+        }
+
+        return [fromRow, fromCol, toRow, toCol];
+    }, [selectedRow, selectedCol, vertical]);
+
+    function onClick(row: number, col: number) {
+        let newVertical = vertical;
+        if (selectedRow === row && selectedCol === col) {
+            newVertical = !vertical;
+        }
+        if (newVertical && !props.grid.cells[row][col].dictionaryItems.vertical) {
+            newVertical = false;
+        }
+        if (!newVertical && !props.grid.cells[row][col].dictionaryItems.horizontal) {
+            newVertical = true;
+        }
+        setVertical(newVertical);
+
+        setSelectedRow(row);
+        setSelectedCol(col);
+    }
+
     return (
         <table className="grid">
             <tbody>
@@ -31,7 +88,9 @@ export function GridComponent(props: {
                             return <Cell
                                 key={`col-${col}`}
                                 character={props.grid.cells[row][col].character}
-                                onClick={() => {}} />;
+                                selected={selectedRow === row && selectedCol === col}
+                                highlighted={row >= highlight[0] && row <= highlight[2] && col >= highlight[1] && col <= highlight[3]}
+                                onClick={() => onClick(row, col)} />;
                         })}
                     </tr>
                 ))}
