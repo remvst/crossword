c7e2e870c8dd2b06f20d28e7a87a65d33f7476b4
remvst <remvst@gmail.com>
2024-11-21 23:29:15 -0500

    Save to localStorage

--- client/src/use-dictionary.ts
+++ client/src/use-dictionary.ts
@@ -1,6 +1,8 @@
 import { Dictionary, DictionaryItem } from "@remvst/crossword";
 import { createContext, useContext, useMemo, useState } from "react";
 
+const DICTIONARY_KEY = 'dictionary';
+
 export const DictionaryContext = createContext<ReturnType<typeof createDictionaryContext>>(null);
 
 export function useDictionary() {
@@ -8,12 +10,24 @@ export function useDictionary() {
 }
 
 export function createDictionaryContext() {
-    const [items, setItems] = useState<DictionaryItem[]>([
-        new DictionaryItem('hello', 'a greeting'),
-        new DictionaryItem('baby', 'a tiny human'),
-    ]);
+    const initialItems: DictionaryItem[] = [];
+    try {
+        const parsed = JSON.parse(localStorage.getItem(DICTIONARY_KEY));
+        for (const { word, definition } of parsed) {
+            initialItems.push(new DictionaryItem(word, definition));
+        }
+    } catch (err) {
+        console.error(err);
+    }
 
-    // TODO load from localStorage
+    const [items, setItems] = useState<DictionaryItem[]>(initialItems);
+
+    function save(items: DictionaryItem[]) {
+        localStorage.setItem(DICTIONARY_KEY, JSON.stringify(items.map(item => ({
+            word: item.word,
+            definition: item.definition,
+        }))));
+    }
 
     const dictionary = useMemo(() => {
         const dict = new Dictionary();
@@ -27,6 +41,7 @@ export function createDictionaryContext() {
         const newItems = items.slice();
         newItems.splice(index, 1);
         setItems(newItems);
+        save(newItems);
     }
 
     function updateItem(index: number, update: (item: DictionaryItem) => void) {
@@ -42,6 +57,7 @@ export function createDictionaryContext() {
         newItems[index] = copy;
 
         setItems(newItems);
+        save(newItems);
     }
 
     return {
